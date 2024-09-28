d34b6d85925fcc64a92734746da093130065b222
Mat4m0 <matthias.amon@me.com>
2024-09-28 14:45:07 +0200

    push

--- app.vue
+++ app.vue
@@ -7,11 +7,11 @@
     FUCK
     c
     <UiAccordion type="single" collapsible variant="default" size="default">
-    <UiAccordionItem value="item-1">
-      <UiAccordionTrigger>Is it accessible?</UiAccordionTrigger>
-      <UiAccordionContent>
-        Yes. It adheres to the WAI-ARIA design pattern.
-      </UiAccordionContent>
+      <UiAccordionItem value="item-1">
+        <UiAccordionTrigger>Is it accessible?</UiAccordionTrigger>
+        <UiAccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </UiAccordionContent>
       </UiAccordionItem>
     </UiAccordion>
 
