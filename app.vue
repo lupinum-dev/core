4babaab5f69f46965d6ed11d4a3ab82f9576c33d
Mat4m0 <matthias.amon@me.com>
2024-09-28 09:01:32 +0200

    shadcn works

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <UiButton variant="outline">Click me</UiButton>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+
+
+    shadcn: {
+      /**
+       * Prefix for all the imported component
+       */
+      prefix: 'Ui',
+      /**
+       * Directory that the component lives in.
+       * @default "./components/ui"
+       */
+      componentDir: './components/ui'
+    }
+  })
