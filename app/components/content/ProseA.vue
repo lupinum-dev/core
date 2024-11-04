4c8c612c12b34d280f925e1fe0fa89cd932d69b3
Mat4m0 <matthias.amon@me.com>
2024-11-04 20:33:49 +0100

    wip

--- app/components/content/ProseA.vue
+++ app/components/content/ProseA.vue
@@ -16,30 +16,46 @@ const props = defineProps({
 const { locale } = useI18n()
 const localePath = useLocalePath()
 
-const pageIdx = props.href.split('/').pop()
-const { data: page, error } = await useAsyncData(props.href, () => queryContent(locale.value, 'wiki')
-  .only(['_path'])
-  .where({ _path: { $contains: pageIdx } })
-  .findOne())
+// Check if URL is external
+const isExternalUrl = computed(() => {
+  const url = props.href.toLowerCase()
+  return url.startsWith('http') || url.startsWith('www.')
+})
+
+// Only fetch data for internal URLs
+const { data: page, error } = await useAsyncData(
+  props.href,
+  async () => {
+    if (isExternalUrl.value) return null
+    
+    const pageIdx = props.href.split('/').pop()
+    return queryContent(locale.value, 'wiki')
+      .only(['_path'])
+      .where({ _path: { $contains: pageIdx } })
+      .findOne()
+  }
+)
 
 const genericPath = computed(() => {
+  if (isExternalUrl.value)
+    return props.href
   if (!page.value || !page.value._path)
     return ''
   return page.value._path.replace(`/${locale.value}/`, '/')
 })
 
-const hasError = computed(() => !page.value || error.value)
+const hasError = computed(() => !isExternalUrl.value && (!page.value || error.value))
 </script>
 
 <template>
   <NuxtLink
     v-if="!hasError"
-    :href="localePath(genericPath)"
+    :href="isExternalUrl ? href : localePath(genericPath)"
     :target="props.target"
   >
     <slot />
   </NuxtLink>
   <span v-else class="rounded-md bg-red-500 p-2 text-white">
-    Error: Invalid link: {{ props.href }},
+    Error: Invalid link: {{ props.href }}
   </span>
 </template>
