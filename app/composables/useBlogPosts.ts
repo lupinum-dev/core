45c3a3128d2506d6c0c20f5fc5b70c2740ef583c
Mat4m0 <matthias.amon@me.com>
2024-12-03 20:50:55 +0100

    wip

--- app/composables/useBlogPosts.ts
+++ app/composables/useBlogPosts.ts
@@ -8,7 +8,7 @@ export function useBlogPosts() {
   const { data: blogPosts } = useAsyncData('posts', () =>
     queryContent(locale.value, 'blog')
       .where({ _partial: false, _draft: false })
-      .only(['title', 'description', '_path', 'category', 'date_published', 'date_modified', 'readTime', 'hero_image'])
+      .only(['title', 'description', '_path', 'category_blog', 'date_published', 'date_modified', 'readTime', 'hero_image'])
       .sort({ date_published: -1 })
       .find()
   )
@@ -17,7 +17,7 @@ export function useBlogPosts() {
     ref._path = "/blog/" +ref._path?.split('/').slice(2).join('/')
   })
 
-  const categories = computed(() => [...new Set(blogPosts.value.flatMap(post => post.category))])
+  const categories = computed(() => [...new Set(blogPosts.value?.flatMap(post => post.category_blog))])
 
   return {
     blogPosts,
